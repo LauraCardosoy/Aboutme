20f713e3fc5c3f8332e7218dca036b572a7471fb
Laura Cardoso <lauracardosoy@gmail.com>
2023-02-01 13:13:55 +0100

    inicio do app

---
+++ aboutme.js
@@ -0,0 +1,8 @@
+const question = [
+    "who is her name?",
+    "How old are her?",
+    "where her was boarn?",
+    "What is her profession",
+    "what is her status",
+    "what is her charch"
+]
